dbb9b2f569f58745d56ba9731c1e3a03e752eabc
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-04-29 14:34:08 +0200

    print next player or winner

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i=-1) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -16,8 +16,12 @@ function Board() {
     setXIsNext(!xIsNext);
   }
 
+  
+  let status = createStatus();
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -35,7 +39,35 @@ function Board() {
       </div>
     </>
   )
+
+  function createStatus() {
+    const winner = calculateWinner(squares);
+    if (winner) {
+      return "Winner: " + winner;
+    } else {
+      return "Next player: " + (xIsNext ? "X" : "O");
+    }
+  }
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 export default Board
