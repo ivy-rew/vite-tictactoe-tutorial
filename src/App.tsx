62270df873999f774871465aaa1d093b3ceca420
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-04-29 14:25:20 +0200

    state on the parent: Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
+import { useState } from 'react';
 import './game.css';
 import Square from './square';
 
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(no=-1) {
+    const nextSquares = squares.slice();
+    nextSquares[no] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   )
--- src/square.tsx
+++ src/square.tsx
@@ -1,13 +1,8 @@
 import { useState } from "react";
 
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X')
-  }
-  return <button className="square" onClick={handleClick}>{value}</button>;
+function Square({value=null, onSquareClick}) {
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
 export default Square
