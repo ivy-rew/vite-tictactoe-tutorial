4e0cda595512fa1f2847854a0e84dcf8909ed47a
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-04-29 14:52:55 +0200

    add history functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,55 @@ import { useState } from 'react';
 import './game.css';
 import Square from './square';
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+
+function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares: any[]) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        History:
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+function Board({ xIsNext, squares, onPlay }) {
 
   function handleClick(i=-1) {
     if (squares[i] || calculateWinner(squares)) {
@@ -12,8 +58,7 @@ function Board() {
     }
     const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? "X" : "O";
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   
@@ -70,4 +115,4 @@ function calculateWinner(squares: any[]) {
   return null;
 }
 
-export default Board
+export default Game
